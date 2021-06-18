9fcf039c8852b9f7410cfc052977818b3395687d
Glitch (hello-webpage) <none>
2021-06-18 13:57:03 +0000

    🎨🍰 Checkpoint ./script.js:3727674/381 ./index.html:3727674/49

--- script.js
+++ script.js
@@ -4,16 +4,16 @@
 // prints "hi" in the browser's dev tools console
 window.onload = () => {
   const videoElement = document.getElementById("video");
+  const ctx = videoElement.getContext('2d');
   const errorElement = document.getElementById("error");
-  const userHaveCamera = new Promise((resolve, reject) => {
+  const userCamera = new Promise((resolve, reject) => {
     navigator.mediaDevices
       .getUserMedia({ video: true })
       .then(e => {
-        console.info(e);
         if (!e) {
           resolve(false);
         } else {
-          resolve(true);
+          resolve(e);
         }
       })
       .catch(e => {
@@ -22,10 +22,12 @@ window.onload = () => {
   });
 
   async function init() {
-    console.info(await userHaveCamera);
-    if (await !userHaveCamera) {
+    console.info(await userCamera);
+    if (await !userCamera) {
       errorElement.innerText = "カメラにアクセスできませんでした";
     } else {
+      ctx.drawImage(userCamera, 0, 0, videoElement.width, videoElement.height)
+      videoElement.srcObject = await userCamera;
       // create new detector
       var barcodeDetector = new BarcodeDetector({
         formats: ["qr_code"]
