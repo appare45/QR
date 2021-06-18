97aecb25e408bb4ec3bb190047b97f8ab61067fa
Glitch (hello-webpage) <none>
2021-06-18 11:32:11 +0000

    🎷🍙 Checkpoint ./script.js:3727674/10104 ./index.html:3727674/61

--- script.js
+++ script.js
@@ -2,31 +2,41 @@
     to your site with Javascript */
 
 // prints "hi" in the browser's dev tools console
-console.log("hi");
 window.onload = () => {
-  
-  const videoElement = document.getElementById('video')
-  const userHaveCamera = () => {
-    try {
-      console.info(navigator.)
+  const videoElement = document.getElementById("video");
+  const errorElement = document.getElementById("error");
+  const userHaveCamera = new Promise((resolve, reject) => {    
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then(e => {
+        console.info(e);
+        if (!e) {
+          resolve(false);
+        } else {
+          resolve(true);
+        }
+      })
+      .catch((e) => {
+        reject(e);
+      });
+  })
+
+  async function init() {
+    if (await !userHaveCamera) {
+      errorElement.innerText = "カメラにアクセスできませんでした";
+    } else {
+      // create new detector
+      var barcodeDetector = new BarcodeDetector({
+        formats: ["qr_code"]
+      });
+
+      // check compatibility
+      if (barcodeDetector) {
+        console.log("Barcode Detector supported!");
+      } else {
+        console.log("Barcode Detector is not supported by this browser.");
+      }
     }
   }
-  
-//   access to user's camera
-  
-  
-  
-  // create new detector
-  var barcodeDetector = new BarcodeDetector({
-    formats: ["qr_code"]
-  });
-
-  // check compatibility
-  if (barcodeDetector) {
-    console.log("Barcode Detector supported!");
-  } else {
-    console.log("Barcode Detector is not supported by this browser.");
-  }
-  
-  
+  init()
 };
