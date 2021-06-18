4501a4919e0323e8390ae7fa9e868c0996831902
Glitch (hello-webpage) <none>
2021-06-18 11:20:14 +0000

    📍💨 Checkpoint ./script.js:3727674/1699 ./index.html:3727674/1993 ./style.css:3727674/62

--- script.js
+++ script.js
@@ -5,23 +5,28 @@
 console.log("hi");
 window.onload = () => {
   
-  // create new detector
-var barcodeDetector = new BarcodeDetector({formats: ['code_39', 'codabar', 'ean_13']});
-
-// check compatibility
-if (barcodeDetector) {
-  console.log('Barcode Detector supported!');
-} else {
-  console.log('Barcode Detector is not supported by this browser.');
-}
-  
-  if (window.BarcodeDetector == undefined) {
-    console.log("No navigator.bluetooth found.");
-  } else {
-    const barcodeDetactor = new window.BarcodeDetector()
-    console.info("Bluetooth check: OK");
+  const videoElement = document.getElementById('video')
+  const userHaveCamera = () => {
+    try {
+      console.info(navigator.)
+    }
   }
-  navigator.permissions
-  .query({ name: "bluetooth" })
-  .then(result => console.log(result));
+  
+//   access to user's camera
+  
+  
+  
+  // create new detector
+  var barcodeDetector = new BarcodeDetector({
+    formats: ["qr_code"]
+  });
+
+  // check compatibility
+  if (barcodeDetector) {
+    console.log("Barcode Detector supported!");
+  } else {
+    console.log("Barcode Detector is not supported by this browser.");
+  }
+  
+  
 };
