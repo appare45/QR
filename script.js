88bba0512eadbe57e24e08e3fc69bbb3b0a428ce
Glitch (hello-webpage) <none>
2021-06-18 11:08:58 +0000

    🎣🏚 Checkpoint ./index.html:3727674/123 ./script.js:3727674/694

--- script.js
+++ script.js
@@ -4,9 +4,21 @@
 // prints "hi" in the browser's dev tools console
 console.log("hi");
 window.onload = () => {
-  if (navigator.bluetooth == undefined) {
+  
+  // create new detector
+var barcodeDetector = new BarcodeDetector({formats: ['code_39', 'codabar', 'ean_13']});
+
+// check compatibility
+if (barcodeDetector) {
+  console.log('Barcode Detector supported!');
+} else {
+  console.log('Barcode Detector is not supported by this browser.');
+}
+  
+  if (window.BarcodeDetector == undefined) {
     console.log("No navigator.bluetooth found.");
   } else {
+    const barcodeDetactor = new window.BarcodeDetector()
     console.info("Bluetooth check: OK");
   }
   navigator.permissions
